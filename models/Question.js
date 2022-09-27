8a082051c7a4b027d0f238592c41a4725fce90df
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2022-09-27 22:34:28 +0300

    Question model created

---
+++ models/Question.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const QuestionSchema = new mongoose.Schema({
+    text: {
+      type: String,
+      required: true
+    },
+    options: {
+      type: [{ option: String, isCorrect: Boolean }],
+      required: true
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required:true
+    },
+    difficulty: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Difficulty",
+      required:true
+    },
+  },{ versionKey: false }
+);
+
+const Question = mongoose.model("Question", QuestionSchema);
+module.exports = Question;
