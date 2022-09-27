a3fe932b471af7e7834813dea2e7f49feb17ad82
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2022-09-27 22:18:22 +0300

    Category model created

---
+++ models/Category.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const CategorySchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },{ versionKey: false }
+);
+
+const Category = mongoose.model("Category", CategorySchema);
+module.exports = Category;
